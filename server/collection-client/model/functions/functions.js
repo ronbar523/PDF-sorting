e16efba867434c9250de99007f96d37b8275e723
Ron Berrbi <100045217+ronbar523@users.noreply.github.com>
2023-08-29 11:10:55 +0300

    Update functions.js

--- server/collection-client/model/functions/functions.js
+++ server/collection-client/model/functions/functions.js
@@ -2,7 +2,7 @@ const { Client } = require("../model");
 const linkedinRegex = /https:\/\/www\.linkedin\.com\/in\/([a-zA-Z0-9-]+)/;
 const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/;
 const idRegex = /\b\d{9}\b/g;
-const phoneNumberRegex = /\b(\d{3}-\d{3}-\d{4}|\d{10})\b/g;
+const phoneNumberRegex = /\b(\d{3}-\d{3}-\d{4}|\d{3}-\d{7}|\d{10})\b/g;
 
 const linkedinMatch = (extractedText) => {
   const linkedin = extractedText.match(linkedinRegex)[0];
